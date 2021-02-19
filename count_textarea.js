b002db756409b7d896e7834f7a31d3c84e0293fb
hidems <miura.eleven@gmail.com>
2021-02-19 12:22:33 +0100

    count remaining number and add color

--- count_textarea.js
+++ count_textarea.js
@@ -1,9 +1,21 @@
-var example1 = new Vue({
+var vm = new Vue({
   el: '#app',
   data: {
-    test: 'Vue.js',
+    postcomment: ''
   },
-  methods: {
-    
+  computed: {
+    textAreaLength: function () {
+        return 150 - this.postcomment.length;
+    },
+    computedColor: function(){
+      col = '#33a';
+      if(this.textAreaLength < 20){
+        col = '#3a3';
+      }
+      if(this.textAreaLength < 1){
+        col = '#a33';
+      }
+      return col;
+    }
   }
 })
